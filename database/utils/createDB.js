bc469670ddaa6c1b57642abefdb8f163cd0a6fe8
Andrew Alagna <aalagna04@gmail.com>
2022-05-10 07:19:27 -0400

    feat: heroku credentials

--- database/utils/createDB.js
+++ database/utils/createDB.js
@@ -3,10 +3,10 @@ const {dbName, dbUser, dbPwd} = require('./configDB');
 
 
 const config = {
-  user: zttvyqdqyosecx,//process.env.PG_USER,
+  user: 'zttvyqdqyosecx',//process.env.PG_USER,
   host: 'ec2-3-229-161-70.compute-1.amazonaws.com',
   port: 5432,
-  password: f7ca7c2417682b5bd06ee550a3c432b29c9d225e0e922f8f030c25d9a2ecf5eb,
+  password: 'f7ca7c2417682b5bd06ee550a3c432b29c9d225e0e922f8f030c25d9a2ecf5eb',
 };
   
   
