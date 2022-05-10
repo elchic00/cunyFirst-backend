e0638718a513b7262a55a2a8943c87b41cc881dd
Andrew Alagna <aalagna04@gmail.com>
2022-05-10 09:20:15 -0400

    feat: connect backend to heroku

--- database/utils/createDB.js
+++ database/utils/createDB.js
@@ -1,8 +1,8 @@
 const pgtools = require('pgtools');
-const {dbName, dbUser, dbPwd} = require('./configDB');
+const { dbName, dbUser, dbPwd } = require('./configDB');
+
 require("dotenv").config();
  
-
 const config = {
     user: process.env.PG_DB,
     host: process.env.PG_HOST,
@@ -10,17 +10,26 @@ const config = {
     password: process.env.PG_PASSWORD
 };
   
-  
+          const { Pool } = require("pg");
+
   //attempt to create DB
   //if it already exists, this does nothing and just connects to
   //the existing db of that name
   const createDB = async () => {
-      try {
-          await pgtools.createdb(config, dbName);
-          console.log(`Successfully created the database: ${dbName}!`);
+    try {
+
+      const pool = new Pool({
+        connectionString:
+          process.env.DATABASE_URL ||
+          "postgresql://postgres:drew@localhost:5432/final-backend",
+        ssl: process.env.DATABASE_URL ? true : false,
+      });
+        pool.connect();
+          // await pgtools.createdb(config, process.env.PG_DB);
+          // console.log(`Successfully created the database: ${process.env.PG_DB}!`);
         } catch (err) {
           if (err.name === 'duplicate_database') {
-              console.log(`Database ${dbName} already exists`);
+              console.log(`Database ${process.env.PG_DB} already exists`);
               return;
             } else {
               console.error('createDB error:', err);
