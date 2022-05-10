f9b96c81fc32446fdc65529bbac5fce1c32c6663
Andrew Alagna <aalagna04@gmail.com>
2022-05-10 07:04:26 -0400

    feat: add credentials

--- database/utils/createDB.js
+++ database/utils/createDB.js
@@ -3,10 +3,10 @@ const {dbName, dbUser, dbPwd} = require('./configDB');
 
 
 const config = {
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
+  user: zttvyqdqyosecx,//process.env.PG_USER,
+  host: 'ec2-3-229-161-70.compute-1.amazonaws.com',
   port: 5432,
-  password: process.env.PG_PASSWORD,
+  password: f7ca7c2417682b5bd06ee550a3c432b29c9d225e0e922f8f030c25d9a2ecf5eb,
 };
   
   
