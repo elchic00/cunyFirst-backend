ab1764c082dc66c8d25c619ee14010ecb73b0e20
Andrew Alagna <aalagna04@gmail.com>
2022-05-12 13:56:29 -0400

    refactor: seed data

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -5,13 +5,14 @@ const seedDB = async () => {
     firstname: "Melissa",
     lastname: "Lynch",
     department: "Computer Science",
-    imageUrl:"https://thumbs.dreamstime.com/b/print-badass-stamp-white-background-labels-stickers-series-142797021.jpg",
+    imageUrl: "https://avatars.githubusercontent.com/u/14899603?v=4",
   });
 	const dummyInstructor2 = await Instructor.create({
     firstname: "Bat",
     lastname: "Man",
     department: "Computer Science",
-    imageUrl:'https://thumbs.dreamstime.com/b/print-badass-stamp-white-background-labels-stickers-series-142797021.jpg'
+    imageUrl:
+      "https://www.indiewire.com/wp-content/uploads/2021/08/Robert-Pattinson-Batman.jpeg?w=780",
   });
 
 	const dummyCourse = await Course.create({
@@ -25,8 +26,15 @@ const seedDB = async () => {
       timeslot: "M 5:35 - 8:15 PM",
     });
 
+  	const dummyCourse3 = await Course.create({
+      title: "CSCI 5000",
+      location: "HN1000",
+      timeslot: "T 5:35 - 8:15 PM",
+    });
+
 	await dummyCourse.setInstructor(dummyInstructor);
 	await dummyCourse2.setInstructor(dummyInstructor2);
+	await dummyCourse3.setInstructor(dummyInstructor2);
 
 	
 }
